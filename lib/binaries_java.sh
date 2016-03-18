09262b7ab6f473293b65e6ae92f489d6dd8cd27d
psyntium <psyntium@hotmail.com>
2016-03-19 05:02:15 +0800

    minor updates

--- lib/binaries_java.sh
+++ lib/binaries_java.sh
@@ -5,7 +5,7 @@ install_java() {
   local download_url="http://download.oracle.com/otn-pub/java/jdk/8u73-b02/jdk-8u73-linux-x64.tar.gz"
   echo "Downloading JAVA [$download_url]"
   curl  --silent --fail --retry 5 --retry-max-time 15 -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" "$download_url" -o /tmp/java.tar.gz || (echo "Unable to download java; does it exist?" && false)
-  echo "Download complete"
+  echo "Download complete!"
 
   echo "Installing JAVA"
   mkdir /tmp/jdk
@@ -13,5 +13,6 @@ install_java() {
   tar xzf /tmp/java.tar.gz -C /tmp/jdk
   rm -rf $dir/*
   mv /tmp/jdk/jdk1.8.0_73/* $dir
-  chmod +x $dir/bin	
+  chmod +x $dir/bin
+  echo "Installation complete!"	
 }
