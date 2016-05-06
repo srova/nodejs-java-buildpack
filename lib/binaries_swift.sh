44502198f8ffc8f148177f0c492abea1cfe8403a
Syahrul Aiman Shaharuddin <syahrul.aiman@my.ibm.com>
2016-05-06 16:08:01 +0800

    skipped clang temporary

--- lib/binaries_swift.sh
+++ lib/binaries_swift.sh
@@ -6,23 +6,21 @@ install_swift() {
   SWIFT_VERSION=2.2.1-RELEASE
   SWIFT_VERSION_=2.2.1-release
   
-  if [[ ! -d "/tmp/clang-$CLANG_VERSION" ]]; then
-    mkdir -p "/tmp/clang-$CLANG_VERSION"
-    local download_url="http://llvm.org/releases/$CLANG_VERSION/clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-14.04.tar.xz"
-    echo "Downloading clang [$download_url]"
-    curl  --silent --fail --retry 5 --retry-max-time 15 -j -k -L "$download_url" -o /tmp/clang.tar.xz || (echo "Unable to download clang; does it exist?" && false)
-    echo "Download complete!"
-    
-    echo "Installing clang"
-    mkdir -p /tmp/clang
-    mkdir -p $dir/clang-$CLANG_VERSION
-    xz -d -c /tmp/clang.tar.xz | tar x -C /tmp/clang
-    rm -rf $dir/clang-$CLANG_VERSION/*
-    mv /tmp/clang/clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-14.04/* $dir/clang-$CLANG_VERSION
-    chmod +x "$dir/clang-$CLANG_VERSION/bin" $dir/clang-$CLANG_VERSION
-    export PATH="$dir/clang-$CLANG_VERSION/bin:$PATH"
-    echo "Installation complete!"
-  fi
+#  local download_url="http://llvm.org/releases/$CLANG_VERSION/clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-14.04.tar.xz"
+#  echo "Downloading clang [$download_url]"
+#  curl  --silent --fail --retry 5 --retry-max-time 15 -j -k -L "$download_url" -o /tmp/clang.tar.xz || (echo "Unable to download clang; does it exist?" && false)
+#  echo "Download complete!"
+#    
+#  echo "Installing clang"
+#  mkdir -p /tmp/clang
+#  mkdir -p $dir/clang-$CLANG_VERSION
+#  xz -d -c /tmp/clang.tar.xz | tar x -C /tmp/clang
+#  rm -rf $dir/clang-$CLANG_VERSION/*
+#  mv /tmp/clang/clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-14.04/* $dir/clang-$CLANG_VERSION
+#  chmod +x "$dir/clang-$CLANG_VERSION/bin" $dir/clang-$CLANG_VERSION
+#  export PATH="$dir/clang-$CLANG_VERSION/bin:$PATH"
+#  echo "Installation complete!"
+  
   
   local download_url="https://swift.org/builds/swift-$SWIFT_VERSION_/ubuntu1404/swift-$SWIFT_VERSION/swift-$SWIFT_VERSION-ubuntu14.04.tar.gz"
   echo "Downloading Swift [$download_url]"
