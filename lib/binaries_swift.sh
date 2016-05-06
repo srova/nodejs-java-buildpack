7f1ace4c867f0590e524cfbc302021c58008bd18
Syahrul Aiman Shaharuddin <syahrul.aiman@my.ibm.com>
2016-05-06 17:47:33 +0800

    debug clang

--- lib/binaries_swift.sh
+++ lib/binaries_swift.sh
@@ -4,23 +4,7 @@ install_swift() {
   
   CLANG_VERSION=3.7.0
   SWIFT_VERSION=2.2.1-RELEASE
-  SWIFT_VERSION_=2.2.1-release
-  
-  local download_url="http://llvm.org/releases/$CLANG_VERSION/clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-14.04.tar.xz"
-  echo "Downloading clang [$download_url]"
-  curl  --silent --fail --retry 5 --retry-max-time 15 -j -k -L "$download_url" -o /tmp/clang.tar.xz || (echo "Unable to download clang; does it exist?" && false)
-  echo "Download complete!"
-    
-  echo "Installing clang"
-  mkdir -p /tmp/clang
-  mkdir -p $dir/clang
-  xz -d -c /tmp/clang.tar.xz | tar x -C /tmp/clang
-  rm -rf $dir/clang/*
-  mv /tmp/clang/clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-14.04/* $dir/clang
-  chmod +x $dir/clang/bin $dir/clang
-#  export PATH="$dir/clang/bin:$PATH"
-  echo "Installation complete!"
-  
+  SWIFT_VERSION_=2.2.1-release  
   
   local download_url="https://swift.org/builds/swift-$SWIFT_VERSION_/ubuntu1404/swift-$SWIFT_VERSION/swift-$SWIFT_VERSION-ubuntu14.04.tar.gz"
   echo "Downloading Swift [$download_url]"
@@ -43,6 +27,19 @@ install_swift() {
   rm -rf $dir/*
   mv /tmp/swift/swift-$SWIFT_VERSION-ubuntu14.04/usr/* $dir
   chmod +x $dir/bin
-#  export PATH="$dir/bin:$PATH"
-  echo "Installation complete!"	
+  echo "Installation complete!"
+    
+  local download_url="http://llvm.org/releases/$CLANG_VERSION/clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-14.04.tar.xz"
+  echo "Downloading clang [$download_url]"
+  curl  --silent --fail --retry 5 --retry-max-time 15 -j -k -L "$download_url" -o /tmp/clang.tar.xz || (echo "Unable to download clang; does it exist?" && false)
+  echo "Download complete!"
+    
+  echo "Installing clang"
+  mkdir -p /tmp/clang
+  mkdir -p $dir/clang
+  xz -d -c /tmp/clang.tar.xz | tar x -C /tmp/clang
+  mv /tmp/clang/clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-14.04/* $dir/clang
+  chmod +x $dir/clang/bin $dir/clang
+  echo "Installation complete!"
+  	
 }
