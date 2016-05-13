98a79412d7a5b9975c4dbe1789ec6e812a3be9fc
Syahrul Aiman Shaharuddin <syahrul.aiman@my.ibm.com>
2016-05-13 17:08:37 +0800

    debug

--- lib/binaries_maven.sh
+++ lib/binaries_maven.sh
@@ -1,8 +1,9 @@
 install_maven() {
   local version="$1"
   local dir="$2"
+  MAVEN_VERSION=3.3.9
   
-  local download_url="http://www-us.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
+  local download_url="http://www-us.apache.org/dist/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz"
   echo "Downloading Maven [$download_url]"
   curl  --silent --fail --retry 5 --retry-max-time 15 -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" "$download_url" -o /tmp/maven.tar.gz || (echo "Unable to download java; does it exist?" && false)
   echo "Download complete!"
@@ -12,8 +13,7 @@ install_maven() {
   mkdir -p $dir
   tar xzf /tmp/maven.tar.gz -C /tmp/maven
   rm -rf $dir/*
-  mv /tmp/maven/* $dir
-  ls $dir
-  #chmod +x $dir/bin
+  mv /tmp/maven/apache-maven-$MAVEN_VERSION* $dir
+  chmod +x $dir/bin
   echo "Installation complete!"	
 }
