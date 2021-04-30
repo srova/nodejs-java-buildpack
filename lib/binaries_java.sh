6951c517374aea8cf532dc6189a6397a79838222
Stefano Rova <stefano.rova@gmail.com>
2021-04-30 17:26:34 +0200

    Update binaries_java.sh

--- lib/binaries_java.sh
+++ lib/binaries_java.sh
@@ -2,9 +2,9 @@ install_java() {
   local version="$1"
   local dir="$2"
   
-  local download_url="http://download.oracle.com/otn-pub/java/jdk/8u73-b02/jdk-8u73-linux-x64.tar.gz"
+  local download_url="https://download.java.net/openjdk/jdk8u41/ri/openjdk_jre_ri-8u41-b04-linux-i586-14_jan_2020.tar.gz"
   echo "Downloading JAVA [$download_url]"
-  curl  --silent --fail --retry 5 --retry-max-time 15 -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" "$download_url" -o /tmp/java.tar.gz || (echo "Unable to download java; does it exist?" && false)
+  curl  --silent --fail --retry 5 --retry-max-time 15 -j -k -L "$download_url" -o /tmp/java.tar.gz || (echo "Unable to download java; does it exist?" && false)
   echo "Download complete!"
 
   echo "Installing JAVA"
@@ -12,7 +12,7 @@ install_java() {
   mkdir $dir
   tar xzf /tmp/java.tar.gz -C /tmp/jdk
   rm -rf $dir/*
-  mv /tmp/jdk/jdk1.8.0_73/* $dir
+  mv /tmp/jdk/java-se-8u41-ri/* $dir
   chmod +x $dir/bin
   echo "Installation complete!"	
 }
