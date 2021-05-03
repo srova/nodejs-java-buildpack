5435ca4b5c0be0e58c2b6e5e4220a30c9fccbd7a
stefano.rova <Pippero999>
2021-05-03 17:44:53 +0200

    test con java 16

--- lib/binaries_java.sh
+++ lib/binaries_java.sh
@@ -2,7 +2,8 @@ install_java() {
   local version="$1"
   local dir="$2"
 
-  local download_url="https://download.java.net/openjdk/jdk8u41/ri/openjdk-8u41-b04-linux-x64-14_jan_2020.tar.gz"
+  local download_url= "https://download.java.net/openjdk/jdk16/ri/openjdk-16+36_linux-x64_bin.tar.gz"
+  ### "https://download.java.net/openjdk/jdk8u41/ri/openjdk-8u41-b04-linux-x64-14_jan_2020.tar.gz"
   echo "Downloading JAVA [$download_url]"
   curl  --silent --fail --retry 5 --retry-max-time 15 -j -k -L "$download_url" -o /tmp/java.tar.gz || (echo "Unable to download java; does it exist?" && false)
   echo "Download complete!"
@@ -17,7 +18,10 @@ install_java() {
   tar xzf /tmp/java.tar.gz -C /tmp/jdk
   rm -rf $dir/*
 
-  mv /tmp/jdk/java-se-8u41-ri/* $dir
+  # mv /tmp/jdk/java-se-8u41-ri/* $dir
+  mv /tmp/jdk/jdk-16/* $dir
+
+
   chmod +x $dir/bin/*
   echo "Installation complete!"
   ls -l $dir
